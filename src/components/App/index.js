8113f05a5c881e1db370ff89ab595f977324710a
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-07-02 13:46:10 -0300

    feat: created Counter feature

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./styles.css";
 import DisplayCategories from "../DisplayCategories";
 import AddItem from "../AddItem";
+import Counter from "../../features/counter/Counter";
 
 const App = () => {
   return (
     <>
       <AddItem />
+      <Counter />
       <DisplayCategories />
     </>
   );
---
+++ src/features/counter/Counter.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
