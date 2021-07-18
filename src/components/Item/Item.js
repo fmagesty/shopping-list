82e708bb45bdd12352bc03cb170385ff580d8a6c
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-07-18 11:42:12 -0300

    added option to click an item and mark it with strike through

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,12 +7,18 @@ import { useSelector } from "react-redux";
 const Item = () => {
   const input = useSelector((state) => state.inputItem.value);
 
+  const handleClick = (e) => {
+    e.target.classList.toggle("strikeThrough");
+  };
+
   return (
     <div id="item-div">
       {input &&
         input.map((item) => (
           <div key={item}>
-            <span id="item-name">{item}</span>
+            <span id="item-name" onClick={handleClick}>
+              {item}
+            </span>
             <Counter />
             <SelectCategory />
           </div>
