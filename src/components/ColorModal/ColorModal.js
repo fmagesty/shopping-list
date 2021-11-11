310c0fb4ba3af812fc3d82d5e417154317168658
Felipe <fmagesty@gmail.com>
2021-11-10 22:22:20 -0300

    fea: create base ColorSelectorModal

--- src/components/ColorModal/ColorModal.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ColorModal() {
-  return <div></div>;
-}
--- src/components/ColorSelector/ColorSelector.js
+++ src/components/ColorSelector/ColorSelector.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import "./styles.css";
 
 export default function ColorSelector() {
-  const colorOptions = ["red", "green", "blue"];
+  const [show, setShow] = useState(false);
 
-  return <button id="color-selector">#</button>;
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
