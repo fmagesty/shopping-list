a6599fe6d43dbbb7622c5cc7a5c2ddca20169eee
Felipe <fmagesty@gmail.com>
2021-11-13 13:39:27 -0300

    fea: complete functional ColorSelectorDropdown

--- src/components/ColorSelectorModal/ColorSelectorDropdown.js
+++ src/components/ColorSelectorModal/ColorSelectorDropdown.js
@@ -1,48 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import { BsSquare } from "react-icons/bs";
 import "./styles.css";
 
 export default function ColorSelectorDropdown() {
-  let currentBgColor = "black";
+  const [color, setColor] = useState("black");
 
   const handleClick = (selectedColor) => {
-    currentBgColor = selectedColor;
+    setColor(selectedColor);
   };
 
   return (
     <>
+      <BsSquare style={{ background: color }} className="category-color" />
       <DropdownButton
         as={ButtonGroup}
-        title="Color"
-        className="bg-vertical-dropdown-1"
-        style={{ backgroundColor: currentBgColor }}>
+        title="Category"
+        className="bg-vertical-dropdown-1">
         <Dropdown.Item eventKey="1" id="red" onClick={() => handleClick("red")}>
           Red
         </Dropdown.Item>
         <Dropdown.Item
           eventKey="2"
-          id="blue"
-          onClick={() => handleClick("blue")}>
-          Blue
+          id="cyan"
+          onClick={() => handleClick("cyan")}>
+          Cyan
         </Dropdown.Item>
-        <Dropdown.Item eventKey="2" id="green">
+        <Dropdown.Item
+          eventKey="2"
+          id="green"
+          onClick={() => handleClick("green")}>
           Green
         </Dropdown.Item>
-        <Dropdown.Item eventKey="2" id="pink">
-          Pink
+        <Dropdown.Item
+          eventKey="2"
+          id="violet"
+          onClick={() => handleClick("mediumvioletred")}>
+          Violet
         </Dropdown.Item>
-        <Dropdown.Item eventKey="2" id="orange">
+        <Dropdown.Item
+          eventKey="2"
+          id="orange"
+          onClick={() => handleClick("orange")}>
           Orange
         </Dropdown.Item>
-        <Dropdown.Item eventKey="2" id="yellow">
+        <Dropdown.Item
+          eventKey="2"
+          id="yellow"
+          onClick={() => handleClick("yellow")}>
           Yellow
         </Dropdown.Item>
-        <Dropdown.Item eventKey="2" id="brown">
+        <Dropdown.Item
+          eventKey="2"
+          id="brown"
+          onClick={() => handleClick("saddlebrown")}>
           Brown
         </Dropdown.Item>
-        <Dropdown.Item eventKey="2" id="purple">
+        <Dropdown.Item
+          eventKey="2"
+          id="purple"
+          onClick={() => handleClick("purple")}>
           Purple
         </Dropdown.Item>
       </DropdownButton>
