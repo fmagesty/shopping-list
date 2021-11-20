4fefc58bcbde2de5d386878f4667913ef8f6df68
Felipe <fmagesty@gmail.com>
2021-11-20 12:54:36 -0300

    fea: add feature to display the category color as a gradient on the ItemName

--- src/components/ItemName/ItemName.js
+++ src/components/ItemName/ItemName.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./styles.css";
+import { useSelector } from "react-redux";
 
 export default function ItemName(itemName) {
+  const itemList = useSelector((state) => state.addItem.itemList);
   itemName = itemName.itemName;
+
+  const currentItem = itemList.find((item) => item.name === itemName);
+  let selectedColor = currentItem.color;
+
   return (
     <span
       id={itemName}
       className="item-name"
       style={{
-        background:
-          "linear-gradient(180deg, rgba(255, 255, 255, 1) 50%, red 100%)",
+        background: `linear-gradient(180deg, rgba(255, 255, 255, 1) 50%, ${selectedColor} 100%)`,
       }}>
       {itemName}
     </span>
