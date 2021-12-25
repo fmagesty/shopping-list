c4c49742b3ceeb603e4ade1278b78a50c4df8fbc
Felipe <fmagesty@gmail.com>
2021-12-25 12:17:50 -0300

    fea: add edit name icon

--- src/components/ItemName/ItemName.js
+++ src/components/ItemName/ItemName.js
@@ -9,14 +9,24 @@ export default function ItemName(itemName) {
   const currentItem = itemList.find((item) => item.name === itemName);
   let selectedColor = currentItem.color;
 
+  const handleEditItemName = () => {
+    console.log('this is handle edititemname')
+  }
+
+
   return (
+    <>
+
     <span
       id={itemName}
       className="item-name"
       style={{
         background: `linear-gradient(180deg, rgba(255, 255, 255, 1) 50%, ${selectedColor} 100%)`,
-      }}>
+      }}
+      >
       {itemName}
     </span>
+    <i className="bi bi-pencil" onClick={() => handleEditItemName()}></i>
+    </>
   );
 }
