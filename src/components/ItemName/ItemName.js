4416ff969bf97228f81a028646abd9505664ee2c
Felipe <fmagesty@gmail.com>
2022-02-27 15:40:55 -0300

    fix: correct boolean state of isEditing on click

--- src/components/ItemName/ItemName.js
+++ src/components/ItemName/ItemName.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { editItemName, addCurrentItemName } from "../DisplayItems/displayItemsSlice";
 
 export default function ItemName(itemName) {
   itemName = itemName.itemName;
+  const [isEditing, setIsEditing] = useState(false)
   const itemList = useSelector((state) => state.addItem.itemList);
+  const dispatch = useDispatch();
   const currentItems = itemList.find((item) => item.name === itemName);
   let selectedColor = currentItems.color;
 
   const handleEditItemName = (itemName) => {
-    console.log(itemName)
+    setIsEditing(!isEditing)
+    console.log('itemName = ' + itemName, 'isEditing = ' + isEditing)
+    dispatch(addCurrentItemName(itemName));
+    // itemList.find((item) => item.name === itemName)
+    // ? console.log(1, itemList, itemName)
+      // ? alert(`${itemName} is already on your list`)
+      // : console.log(2, itemList, itemName)
+      // : dispatch(editItemName('newname'));
+        <input type="text"
+        name="input-item"
+        id="input-item"
+        placeholder="Item name"
+        onChange={handleChange} />
+  }
+
+  const handleChange = (e) => {
+    itemName = e.target.value;
+    console.log(itemName);
+    setIsEditing(false)
+    return itemName;
   }
 
   return (
@@ -18,7 +40,7 @@ export default function ItemName(itemName) {
       id={itemName}
       className="item-name"
       style={{
-        background: `linear-gradient(180deg, rgba(255, 255, 255, 1) 50%, ${selectedColor} 100%)`,
+        background: `linear-gradient(180deg, rgba(255, 255, 255, 1) 50%, ${selectedColor} 100%`,
       }}
       >
       {itemName}
