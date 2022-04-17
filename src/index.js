d34adb0aeb354f8b626008d44efe75c4435ad9de
Felipe <fmagesty@gmail.com>
2022-04-16 21:05:18 -0300

    enh:lint/format index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "../src/components/App/App";
-import store from "../src/store/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from '../src/components/App/App';
+import store from '../src/store/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById('root')
 );
 
 //test31
